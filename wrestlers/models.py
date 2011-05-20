b1ac3e6b4c5398248bd448c38c7f0953f0aad08d
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-05-21 00:43:18 +0100

    Add basic wrestlers models.

--- wrestlers/models.py
+++ wrestlers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class GroupManager(models.Manager):
+
+    def filter_wrestlers(self, l):
+        return self.filter(wrestlers=Wrestler.objects.filter(id__in=[w.id for w in l]))
+
+class Group(models.Model):
+
+    objects = GroupManager()
+
+    wrestlers = models.ManyToManyField("Wrestler")
+    name = models.CharField(max_length=128)
+
+
+class Wrestler(models.Model):
+
+    name = models.CharField(max_length=128)
