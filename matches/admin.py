e06834c725150f6202c309595c6ebbad54999715
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-05-21 01:31:59 +0100

    Register classes with admin.

---
+++ matches/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from matches.models import Card, Match
+
+
+admin.site.register(Card)
+
+
+admin.site.register(Match)
---
+++ wrestlers/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from wrestlers.models import Group, WrestlingTeam, Wrestler
+
+
+admin.site.register(Group)
+
+
+admin.site.register(WrestlingTeam)
+
+
+admin.site.register(Wrestler)
