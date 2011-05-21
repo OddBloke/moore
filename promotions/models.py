20872aface55d88b169b952abd89ceb5758bcf5d
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-05-21 19:45:48 +0100

    Add models from promotion name history.

--- promotions/models.py
+++ promotions/models.py
@@ -15,19 +15,52 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
-class Promotion(models.Model):
+class PromotionName(models.Model):
 
+    promotion = models.ForeignKey("Promotion", related_name="names")
     name = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
 
-    parent = models.ForeignKey("self", null=True, blank=True)
+    def clean(self):
+        if self.end_date is not None and self.start_date > self.end_date:
+            raise ValidationError("Start date after end date.")
+        self.promotion.clean_fields()
+        if self.start_date < self.promotion.start_date:
+            raise ValidationError("Start date of name before start date of"
+                                  " promotion.")
+        if (self.promotion.end_date is not None
+                and self.start_date > self.promotion.end_date):
+            raise ValidationError("Start date of name after end date of"
+                                  " promotion.")
+        if (self.promotion.end_date is not None
+                and self.end_date is not None
+                and self.end_date > self.promotion.end_date):
+            raise ValidationError("End date of name after end date of"
+                                  " promotion.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(PromotionName, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
+
+
+class Promotion(models.Model):
+
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+
+    def name(self):
+        return self.names.get(end_date__isnull=True).name
+
+    def __unicode__(self):
+        return self.name()
 
 
 class Title(models.Model):
--- promotions/tests.py
+++ promotions/tests.py
@@ -1,16 +1,67 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+# moore - a wrestling database
+# Copyright (C) 2011  Daniel Watkins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-Replace this with more appropriate tests for your application.
-"""
-
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from promotions.models import Promotion, PromotionName
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class PromotionNameTest(TestCase):
+
+    def setUp(self):
+        self.p1 = Promotion.objects.create(start_date="2000-02-01",
+                                           end_date="2000-03-01")
+        self.p2 = Promotion.objects.create(start_date="2000-02-01")
+
+    def assertRaisesValidationError(self, promotion, name, start_date,
+                                    end_date=None):
+        self.assertRaises(ValidationError,
+                          PromotionName.objects.create,
+                          promotion=promotion,
+                          start_date=start_date,
+                          end_date=end_date,
+                          name=name)
+
+    def test_not_older_than_promotion(self):
+        self.assertRaisesValidationError(self.p1, "Pre Name", "2000-01-01")
+        self.assertRaisesValidationError(self.p2, "Pre Name", "2000-01-01")
+
+    def test_not_newer_than_promotion(self):
+        self.assertRaisesValidationError(self.p1, "Post Name", "2000-04-01")
+
+    def test_doesnt_end_after_promotion(self):
+        self.assertRaisesValidationError(self.p1, "After Name", "2000-02-01",
+                                         "2000-04-01")
+
+    def test_start_before_end(self):
+        self.assertRaisesValidationError(self.p1, "After Name", "2000-04-01",
+                                         "2000-02-01")
+        self.assertRaisesValidationError(self.p2, "After Name", "2000-04-01",
+                                         "2000-02-01")
+
+    def test_promotion_name(self):
+        self.assertRaises(PromotionName.DoesNotExist, self.p2.name)
+        n1 = PromotionName.objects.create(promotion=self.p2, name="A",
+                                          start_date="2000-02-01")
+        self.assertEqual("A", self.p2.name())
+        n1.end_date = "2000-03-01"
+        n1.save()
+        self.assertRaises(PromotionName.DoesNotExist, self.p2.name)
+        n2 = PromotionName.objects.create(promotion=self.p2, name="B",
+                                          start_date="2000-03-01")
+        self.assertEqual("B", self.p2.name())
