9e88f0dbbad4880d024c8a41a936b934dcd66de8
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-05-21 12:28:18 +0100

    Test Match string display.

--- matches/tests.py
+++ matches/tests.py
@@ -7,10 +7,48 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from matches.models import Card, Match
+from wrestlers.models import Wrestler
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+class MatchTest(TestCase):
+
+    fixtures = ['test.json']
+
+    def setUp(self):
+        self.card = Card.objects.get(id=1)
+        self.w1 = Wrestler.objects.get(id=1)
+        self.w2 = Wrestler.objects.get(id=2)
+        self.w3 = Wrestler.objects.get(id=3)
+
+    def _create_two_person_match(self):
+        m = Match.objects.create(card=self.card)
+        m.save()
+        m.participants.add(self.w1)
+        m.participants.add(self.w2)
+        m.save()
+        return m
+
+    def test_vs_string(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that the vs. string works for different numbers of
+        wrestlers.
         """
-        self.assertEqual(1 + 1, 2)
+        m = self._create_two_person_match()
+        self.assertEqual("%s vs. %s" % (self.w1.name, self.w2.name),
+                         m.vs_string())
+        m.participants.add(self.w3)
+        m.save()
+        self.assertEqual("%s vs. %s vs. %s" % (self.w1.name,
+                                               self.w2.name,
+                                               self.w3.name),
+                         m.vs_string())
+
+    def test_unicode(self):
+        m = self._create_two_person_match()
+        self.assertEqual("%s: %s" % (self.card.date, m.vs_string()),
+                         unicode(m))
+        m.participants.add(self.w3)
+        m.save()
+        self.assertEqual("%s: %s" % (self.card.date, m.vs_string()),
+                         unicode(m))
