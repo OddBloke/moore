7baaa2b5ba78df1ce785d1ff21c6c8e432e62bc3
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-05-21 02:44:40 +0100

    Don't require an end date for promotions (as they may still be going).

--- promotions/models.py
+++ promotions/models.py
@@ -5,7 +5,7 @@ class Promotion(models.Model):
 
     name = models.CharField(max_length=255)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
 
     parent = models.ForeignKey("self", null=True, blank=True)
 
